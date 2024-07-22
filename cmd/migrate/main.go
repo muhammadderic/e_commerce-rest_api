cb5a377280ecf26e091797979b076edf65d99418
muhammadderic <muhammad.donny.e@gmail.com>
2024-07-22 13:10:41 +0700

    Create migration handler file and migrate users tables

---
+++ cmd/migrate/main.go
@@ -0,0 +1,54 @@
+package migrate
+
+import (
+	"log"
+	"os"
+
+	mysqlDrive "github.com/go-sql-driver/mysql"
+	"github.com/golang-migrate/migrate/v4"
+	mysqlMigrate "github.com/golang-migrate/migrate/v4/database/mysql"
+	"github.com/muhammadderic/ecomrest/configs"
+	"github.com/muhammadderic/ecomrest/db"
+)
+
+func main() {
+	db, err := db.NewMySQLStorage(mysqlDrive.Config{
+		User:                 configs.Envs.DBUser,
+		Passwd:               configs.Envs.DBPassword,
+		Addr:                 configs.Envs.DBAddress,
+		DBName:               configs.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	driver, err := mysqlMigrate.WithInstance(db, &mysqlMigrate.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://cmd/migrate/migrations",
+		"mysql",
+		driver,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmd := os.Args[(len(os.Args) - 1)]
+
+	if cmd == "up" {
+		if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+	}
+	if cmd == "down" {
+		if err := m.Down(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+	}
+}
